6b9f98350336bceb645f247693d388a680b38b0e
Kim Atle Norén <kim.noren@statnett.no>
2018-03-08 17:29:36 +0100

    Renamed tests

--- src/test/java/BogusTest.java
+++ src/test/java/BogusTest.java
@@ -1,7 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Bogus {
+public class BogusTest {
 
     @Test
     public void bogus() {
--- src/test/java/PositiveTest.java
+++ src/test/java/PositiveTest.java
@@ -1,7 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PositiveTests {
+public class PositiveTest {
 
     @Test
     public void isPositive(){
